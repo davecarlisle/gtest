102a2de954dfd000ee8efe8410ea32dba5709014
Dave Carlisle <dave.carlisle@hpe.com>
2024-03-01 20:43:21 -0800

    Init...

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { Grommet, Collapsible, Box, Card, CardHeader, Image, CardFooter, CardBody, Heading, Text, Button, Grid } from 'grommet';
+import { hpe } from 'grommet-theme-hpe';
+import { Favorite } from 'grommet-icons';
+import { Header, Sidebar, Footer, Notification } from './components';
+import React, { useState } from "react";
+
+function App() {
+
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  return (
+    <Grommet className="App" theme={hpe} full>
+
+      <Box fill> {/* new box */}
+        <Box elevation="medium">
+           <Header />
+        </Box>
+
+        <Box direction='row' flex>
+          <Collapsible direction="horizontal" open={showSidebar}>
+            <Sidebar />
+          </Collapsible>
+          <Box margin={{ top: "10px" }} flex>
+            <Notification/>
+            <Box align="center" justify="center" flex>
+            <Card height="medium" width="medium">
+              <CardHeader background="brand">
+              <Box flex>
+                <Image fill="true" src="./images/NewsImage.jpg"/> 
+              </Box>
+              </CardHeader>
+              <CardBody pad="large" align="center">
+                <Text size="large">Coming soon...</Text>
+              </CardBody>
+              <CardFooter pad={{horizontal: "small"}} background="background-contrast">
+                <Button color="brand" icon={<Favorite />}hoverIndicator/>
+              </CardFooter>
+            </Card>
+            </Box>
+          </Box>
+        </Box>
+
+        <Box elevation="medium">
+          <Footer />
+        </Box>
+
+      </Box>
+
+    </Grommet>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,85 @@
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+import {
+  Box,
+  Button,
+  Header,
+  Keyboard,
+  ResponsiveContext,
+  Text,
+  Nav,
+  Anchor,
+  TextInput,
+  Tip
+} from 'grommet';
+import { Search as SearchIcon, Hpe, User, Slack, HelpOption, Notification} from 'grommet-icons';
+
+const StyledTextInput = styled(TextInput).attrs(() => ({
+  'aria-labelledby': 'search-icon-example',
+}))``;
+
+const AppHeader = () => {
+  const size = useContext(ResponsiveContext);
+  const [focused, setFocused] = useState(false);
+  const inputRef = useRef();
+
+  useEffect(() => {
+    if (focused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [focused, setFocused]);
+
+  return (
+    <Header
+      fill="horizontal"
+      pad={{ horizontal: 'medium', vertical: 'small' }}
+      background="background-front"
+    >
+      <Tip content="Navigate to Home Page">
+      <Button>
+        <Box
+          direction="row"
+          align="start"
+          gap="medium"
+          // pad maintains accessible hit target
+          // non-responsive maintains same dimensions for mobile
+          pad={{ vertical: 'small' }}
+          responsive={false}
+        >
+          <Hpe color="brand" />
+          {(!['xsmall', 'small'].includes(size) ||
+            (['xsmall', 'small'].includes(size) && !focused)) && (
+            <Box direction="row" gap="xsmall" wrap>
+              <Text color="text-strong" weight="bold">
+                HPE
+              </Text>
+              <Text color="text-strong">Status Central</Text>
+            </Box>
+          )}
+        </Box>
+      </Button>
+      </Tip>
+
+      <Nav direction="row" gap="small">
+        <Button label="Home" />
+        <Button label="Support" />
+      </Nav>
+
+      <Nav direction="row" gap="xsmall">
+        <Tip content="No ongoing notifications at this time">
+          <Anchor tip="test" icon={<Notification />} hoverIndicator />
+        </Tip>
+        <Tip content="Join the overall Slack support channel">
+          <Anchor color="plain" icon={<Slack />} hoverIndicator />
+        </Tip>
+        <Tip content="Additional help information">  
+          <Anchor icon={<HelpOption />} hoverIndicator />
+        </Tip>
+        <Anchor icon={<User />} hoverIndicator />
+      </Nav>
+
+    </Header>
+  );
+};
+
+export { AppHeader as Header };
